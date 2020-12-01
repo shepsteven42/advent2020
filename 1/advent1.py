789dc81fc3eb84b860af31e80f5a1a52872c0cf3
shepsteven42 <53957731+shepsteven42@users.noreply.github.com>
2020-11-30 23:43:19 -0800

    Add files via upload

---
+++ 1/advent1.py
@@ -0,0 +1,8 @@
+l,p=[int(x) for x in open("i.t").read().split()],print
+for i in l:
+	for j in [t for t in l if t==2020-i]:
+		p(i*j)
+for i in l:
+	for j in [t for t in l if t<2020-i]:
+		for k in [t for t in l if t==2020-i-j]:
+			p(i*j*k)
