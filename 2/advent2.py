b1645ab5668b8c0e9f1a0ddba9464768f91e1921
shepsteven42 <53957731+shepsteven42@users.noreply.github.com>
2020-12-08 20:58:58 -0800

    Update advent2.py

--- 2/advent2.py
+++ 2/advent2.py
@@ -1,2 +1,2 @@
-f,s=[[int(x[0]),int(x[1]),[i==x[2][0] for i in x[3]]] for x in [x.replace('-',' ').split() for x in open("i.t")]],sum
+f,s=[[int(x[0]),int(x[1]),[i==x[2][0] for i in x[3]]] for x in[x.replace('-',' ').split() for x in open("i.t")]],sum
 print(s([x[0]<=s(x[2])<=x[1] for x in f]),s([x[2][x[0]-1]^x[2][x[1]-1] for x in f]))
