9c1ae9694e450e2aeb1474873a907ef47d964f0c
shepsteven42 <53957731+shepsteven42@users.noreply.github.com>
2020-12-08 21:16:09 -0800

    Update advent1.py

--- 1/advent1.py
+++ 1/advent1.py
@@ -1,8 +1,2 @@
-l,p=[int(x) for x in open("i.t")],print
-for i in l:
-	for j in [t for t in l if t==2020-i]:
-		p(i*j)
-for i in l:
-	for j in [t for t in l if t<2020-i]:
-		for k in [t for t in l if t==2020-i-j]:
-			p(i*j*k)
+l=[int(x) for x in open("i.t")]
+print([i*j for i in l for j in l if i+j==2020],[i*j*k for i in l for j in l for k in l if i+j+k==2020])
