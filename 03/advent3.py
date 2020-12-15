d5e9593915bf01bd9b60309bfca821320b4e7076
shepsteven42 <53957731+shepsteven42@users.noreply.github.com>
2020-12-14 20:57:13 -0800

    Update advent3.py

--- 03/advent3.py
+++ 03/advent3.py
@@ -1,6 +1,6 @@
-f,t,p=[x.strip()for x in open("i.t")],1,print
-for k in[[3,1],[1,1],[5,1],[7,1],[1,2]]:
-	y=x=c=0
-	while y<len(f):c+=f[y][x%len(f[0])]=='#';y+=k[1];x+=k[0]
-	p(c);t*=c
-p(t)
+f,q,p=[x.strip()for x in open("i.t")],1,print
+for a,b in zip([3,1,5,7,1],[1,1,1,1,2]):
+	t=c=0
+	while b*t<len(f):c+=f[b*t][a*t%len(f[0])]=='#';t+=1
+	p(c);q*=c
+p(q)
